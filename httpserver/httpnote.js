0e08187ab297ccd964af88199d579fd7f64d4ede
jesse h <jesselevihall@gmail.com>
2022-09-30 11:47:49 -0600

    local server

---
+++ httpserver/httpnote.js
@@ -0,0 +1,4 @@
+//post method is used to send data to the server to a collection of resources.
+//put method is used to send data to the server to a specific resource.
+
+//req is a readable stream. res is a writable stream.
