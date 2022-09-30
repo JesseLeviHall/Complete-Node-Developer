f236f4ac52a45f26ad48cdf02b1b09d0fc96ec6a
jesse h <jesselevihall@gmail.com>
2022-09-30 12:50:39 -0600

    endpoints

--- httpserver/httpnote.js
+++ httpserver/httpnote.js
@@ -2,3 +2,5 @@
 //put method is used to send data to the server to a specific resource.
 
 //req is a readable stream. res is a writable stream.
+
+//each url is an enc point, every time a request is made to the server, the callback function is called.
--- httpserver/index.js
+++ httpserver/index.js
@@ -3,8 +3,13 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!");
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      id: 1,
+      message: "Hello World",
+    })
+  );
 });
 
 server.listen(PORT, () => {
