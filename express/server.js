97bd8a38f6227d848519298bac4f471b2fd2de37
jesse h <jesselevihall@gmail.com>
2022-10-01 12:20:59 -0600

    friend request

--- express/server.js
+++ express/server.js
@@ -4,6 +4,26 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  { id: 0, name: "John" },
+  { id: 1, name: "Jane" },
+  { id: 2, name: "Jim" },
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.json(friend);
+  } else {
+    res.status(404).json({ error: "Friend not found" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
