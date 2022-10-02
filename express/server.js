2db23ebb81c8c24bc7a1b42a3e88922370872841
jesse h <jesselevihall@gmail.com>
2022-10-01 18:25:00 -0600

    post new friend

--- express/server.js
+++ express/server.js
@@ -21,6 +21,17 @@ app.use((req, res, next) => {
   console.log(`Request IP: ${req.url} Method: ${req.method}`);
 });
 
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ msg: "Please include a name" });
+  }
+  const newFriend = { name: req.body.name, id: friends.length };
+  friends.push(newFriend);
+  res.json(newFriend);
+});
+
 app.get("/friends/:friendId", (req, res) => {
   const friendId = Number(req.params.friendId);
   const friend = friends[friendId];
