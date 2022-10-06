44e13f63f3a0a99109b42c4a8a637de58be0821d
jesse h <jesselevihall@gmail.com>
2022-10-06 17:57:30 -0600

    plantes route

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
+const planetsRouter = require("./routes/planets.router");
+
 const app = express();
 app.use(express.json());
-
+app.use(planetsRouter);
 module.exports = app;
