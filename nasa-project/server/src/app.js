b1f44fa5df82b3124427c1226d69714e171979d8
jesse h <jesselevihall@gmail.com>
2022-10-07 10:53:44 -0600

    add cors

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const planetsRouter = require("./routes/planets.router");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(planetsRouter);
 module.exports = app;
