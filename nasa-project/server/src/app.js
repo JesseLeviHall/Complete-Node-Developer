d9830f67ef12caa0431d0117681bf0996d079650
jesse h <jesselevihall@gmail.com>
2022-10-10 17:52:55 -0600

    serving client side in express

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -11,5 +12,6 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
 module.exports = app;
