3cc9193b4713f2d0d57f6d34046425416688fa7b
jesse h <jesselevihall@gmail.com>
2022-10-12 17:17:56 -0600

    post launches

--- nasa-project/server/src/models/launches.model.js
+++ nasa-project/server/src/models/launches.model.js
@@ -1,6 +1,8 @@
 //maps are a data structure that allows you to store key value pairs in a specific order.
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -15,10 +17,25 @@ const launch = {
 //the set method allows you to add a new key value pair to the map:
 launches.set(launch.flightNumber, launch);
 
+//this is a data layer function that returns all the launches in the map: it allows us to keep the data layer separate from the business logic layer.
 function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    launch.flightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["Zero To Mastery", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
