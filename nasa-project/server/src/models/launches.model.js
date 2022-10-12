8fb345e6b0c8398e21eec17e3f87ccbbecca171f
jesse h <jesselevihall@gmail.com>
2022-10-12 13:45:11 -0600

    launches endpoint

---
+++ nasa-project/server/src/models/launches.model.js
@@ -0,0 +1,20 @@
+//maps are a data structure that allows you to store key value pairs in a specific order.
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  customers: ["NASA", "ZTM"],
+  upcoming: true,
+  success: true,
+};
+
+//the set method allows you to add a new key value pair to the map:
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
