2f9adde03ebd12bd075f86c42b37175d83464a46
jesse h <jesselevihall@gmail.com>
2022-10-13 17:28:37 -0600

    testing get launches endpoint

--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -1,56 +1,23 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
   test("It should respond with 200 success", async () => {
-    const response = await request(app).get("/launches");
-    expect(response.statusCode).toBe(200);
+    const response = await request(app)
+      .get("/launches")
+      .expect(200)
+      .expect("Content-Type", /json/);
   });
 });
 
 describe("Test POST /launches", () => {
-  const completeLaunchData = {
-    mission: "USS Enterprise",
-    rocket: "NCC-1701-D",
-    target: "Kepler-442 b",
-    launchDate: "January 4, 2028",
-  };
-
-  const launchDataWithoutDate = {
-    mission: "USS Enterprise",
-    rocket: "NCC-1701-D",
-    target: "Kepler-442 b",
-  };
-
-  const launchDataWithInvalidDate = {
-    mission: "USS Enterprise",
-    rocket: "NCC-1701-D",
-    target: "Kepler-442 b",
-    launchDate: "zoot",
-  };
-
-  test("It should respond with 201 created", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(completeLaunchData);
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toMatchObject(completeLaunchData);
+  test("It should respond with 200 success", () => {
+    // test code goes here
   });
-
-  test("It should catch missing required properties", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithoutDate);
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toStrictEqual({
-      error: "Missing required launch property",
-    });
+  test("It should catch missing required properties", () => {
+    // test code goes here
   });
-
-  test("It should catch invalid dates", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithInvalidDate);
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toStrictEqual({
-      error: "Invalid launch date",
-    });
+  test("It should catch invalid dates", () => {
+    // test code goes here
   });
 });
