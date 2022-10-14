65c83a7187b0235b56ba4510208beb6f1e766989
jesse h <jesselevihall@gmail.com>
2022-10-14 16:31:18 -0600

    worker threads example

--- improve_node_performance/perf_boost.js
+++ improve_node_performance/perf_boost.js
@@ -1,3 +1,9 @@
 //start server on pm2 with the command pm2 start server.js -i max
 
 //to stop the server use the command pm2 stop server.js and to delete the server use the command pm2 delete server.js
+
+//pm2 start server.js -l logs.txt -i max
+//pm2 show <id>
+//open mac terminal and type pm2 monit
+
+//pm2 zero downtime restart allows for the server to be restarted without any downtime.  This is done by starting a new server and then when the new server is ready, it will replace the old server.  This is done by using the command pm2 reload server.js --update-env
