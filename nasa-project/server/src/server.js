0ab3b8ad5285f74964f5a796407769dae146b61b
jesse h <jesselevihall@gmail.com>
2022-10-16 17:17:02 -0600

    upsert data

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,13 +1,12 @@
 const http = require("http");
 const mongoose = require("mongoose");
+const process = require("process");
 
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
-
-const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/nasa";
 
 const server = http.createServer(app);
 
@@ -21,7 +20,7 @@ mongoose.connection.on("error", (err) => {
 
 async function startServer() {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     await loadPlanetsData();
   } catch (error) {
     console.log(error);
