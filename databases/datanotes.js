e572beb64036904b102fe93c6cfd3afac8c64ab6
jesse h <jesselevihall@gmail.com>
2022-10-15 18:08:24 -0600

    database types

---
+++ databases/datanotes.js
@@ -0,0 +1,17 @@
+/* 
+the five main data base models are: 
+1. relational
+    moslty used for structured data and SQL and tables
+2. document
+    mostly used for unstructured data and NoSQL
+    rather than rows and columns, documents are stored as documents. 
+3. key-value
+  redis is an example of a key-value database
+4. graph
+  neo4j is an example of a graph database. good for data that has relationships, like social networks
+5. wide-column
+  amazon apache is an example of a wide-column database.  good for data that has a lot of columns, like sensor data. 
+
+
+  
+*/
