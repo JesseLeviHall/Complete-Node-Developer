345393df3b78a086c11182ff69f528be48590a7b
jesse h <jesselevihall@gmail.com>
2022-10-18 16:27:03 -0600

    throw error if planet not found

--- nasa-project/server/src/models/launches.model.js
+++ nasa-project/server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
 //maps are a data structure that allows you to store key value pairs in a specific order.
 const launches = new Map();
@@ -28,6 +29,12 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+  if (!planet) {
+    throw new Error("No matching planet was found.");
+  }
   await launchesDatabase.upDateOne(
     {
       flightNumber: launch.flightNumber,
