93e04aec56c3e77704fd1f1b9ac79a104a3aae4c
jesse h <jesselevihall@gmail.com>
2022-10-19 09:19:33 -0600

    launches api tests

--- nasa-project/server/src/services/mongo.js
+++ nasa-project/server/src/services/mongo.js
@@ -1,21 +1,5 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL = "";
-
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log("MongoDB connection error", err);
-});
-
-async function mongoConnect() {
-  mongoose.connect(
-    "mongodb+srv://nasa-api:3EZYIJZUxkbZE2fe@nasacluster.5cg5ecw.mongodb.net/?retryWrites=true&w=majority"
-  );
-}
-
 module.exports = {
   mongoConnect,
 };
