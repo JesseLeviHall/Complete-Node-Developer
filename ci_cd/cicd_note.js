2f282d2e5475c4911e2082b6cdd5708ea673a07c
jesse h <jesselevihall@gmail.com>
2022-10-23 18:31:30 -0600

    yaml actions

---
+++ ci_cd/cicd_note.js
@@ -0,0 +1,12 @@
+/* 
+continuous integration and continuous delivery and continuous deployment. 
+
+CI is the process of automating the build and testing of code every time a developer pushes changes. It part of the agile development process. 
+
+Continuous Delivery automates the release of software. It is the process of automating the build, test, and release of software.
+
+continuous deployment is the process of automatically deploying code to production after it passes a series of tests. means that every change that passes all stages of your production pipeline is released to your customers. its the final target of continuous delivery.
+
+in github actions, the ci.yml file is the file that contains the steps to run the ci process. (workflow configuration file)
+we can use the actions from the marketplace to run the ci process without writing the code ourselves.
+*/
