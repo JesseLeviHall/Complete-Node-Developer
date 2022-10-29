7866cf7d09ba7c0f08042eb2d41d7584cfe588a0
jesse h <jesselevihall@gmail.com>
2022-10-29 13:05:23 -0600

    tsconfig and watch

--- typescript/types_note.ts
+++ typescript/types_note.ts
@@ -1,4 +1,11 @@
 /* 
-javascript is dyamic typed, typescript makes it static typed. 
+javascript is dynamiccally and wealy typed, meaning type coersion, and truthy and falsy. 
 
+typescript makes it statically and strongly typed. It is a jasasctipt superset. 
+
+angular is written in typescript by default. the largest benefit is large projects have less bugs and sort of self document. 
+
+use tsc --init to set up a config file. 
+use tsc <file> --watch to watch for changes and recompile to js output. 
 */
+
